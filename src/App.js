5caab7d89b6a745f3cd13527dfade444347fabbd
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-06-02 16:46:48 -0300

    react+redux simples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+// import React, {useState} from 'react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Intervalo from './components/Intervalo'
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio from './components/Sorteio';
+
 function App() {
+  // const [min, setMin] = useState(1)
+  // const [max, setMax] = useState(10)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Exercício React-Redux (Simples)</h4>
+      <div className="linha">
+        {/* <Intervalo min={min} max={max} onMinChange={setMin} onMaxChange={setMax}></Intervalo> */}
+        <Intervalo></Intervalo>
+      </div>
+
+      <div className="linha">
+        <Media></Media>
+        <Soma></Soma>
+        <Sorteio></Sorteio>
+        {/* <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Card from './Card'
+
+const Sorteio = props => {
+  const { min, max } = props
+  const aleatorio = parseInt(Math.random() * (max - 1)) + min
+
+  return (
+    <Card title="Sorteio de um Número" purple>
+      <div>
+        <span>
+          <span>Resultado:</span>
+          <strong>{aleatorio}</strong>
+        </span>
+      </div>
+    </Card>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Sorteio)
